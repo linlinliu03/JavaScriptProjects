4af042f06294cdfbe2a9f56d56959260c3304c70
linlinliu <linlinliu03@gmail.com>
2020-04-29 10:13:54 -0700

    Add css styles

--- Custom Video Player/scripts.js
+++ Custom Video Player/scripts.js
@@ -25,6 +25,7 @@ function skip(){
 }
 
 function handleRangeUpdate(){
+    console.log(this.value)
     video[this.name] = this.value;
 }
 
@@ -51,10 +52,11 @@ toggle.addEventListener('click',togglePlay);
 skipButtons.forEach(button => button.addEventListener('click', skip))
 
 ranges.forEach(range => range.addEventListener('change', handleRangeUpdate));
-ranges.forEach(range => range.addEventListener('mousemove', handleRangeUpdate));
+// ranges.forEach(range => range.addEventListener('mousemove', handleRangeUpdate));
 
 let mousedown = false;
 progress.addEventListener('click',scrub);
 progress.addEventListener('mousemove', (e) => mousedown && scrub(e));
 progress.addEventListener('mousedown',() => mousedown = true);
 progress.addEventListener('mousemove', () => mousedown = false);
+
